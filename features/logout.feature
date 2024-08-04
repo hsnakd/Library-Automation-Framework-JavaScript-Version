43a588d79861741ac4d2c6343841d4102a30dfdd
hsnakd <hsnakd10@gmail.com>
2024-08-04 22:21:09 +0300

    feature/@lib-01-login completed

--- features/logout.feature
+++ features/logout.feature
@@ -1,12 +1,19 @@
 @lib-02
 Feature: Logout from the application
 
-    As a user, I should be able to logout from the app.
+  As a user, I should be able to logout from the app.
 
-    #* AC1: user should log out from the homepage by clicking the “Log out”  button under the account name.
+  #* AC1: user should log out from the homepage by clicking the “Log out”  button under the account name.
 
+  Background:
+    Given user logs in as "<user-type>"
 
-    # TODO: verify users can logout from the app
+@lib-02-01
+  Scenario Outline: Verify users can log in with valid credentials and log out successfully
+    When user clicks the "Log out" button under the account name
+    Then user is redirected to the login page with the title "Login - Library"
 
-    
-    #? Should there be more scenarios for this user story? Feel free to add more scenarios.
+    Examples:
+      | user-type  |
+      | admin      |
+      | student    |
--- steps/logoutSteps.js
+++ steps/logoutSteps.js
@@ -3,5 +3,34 @@ import { expect } from '@playwright/test';
 import { BrowserUtility } from '../utilities/BrowserUtility.js';
 import { PageManager } from "../globalPagesSetup.js";
 
+// Step Definitions
+Given('user logs in as {string}', async function (userType) {
+  // Go to the login page
+  await PageManager.page.goto(PageManager.loginPage.libraryUrl);
 
-// WRITE YOUR STEP DEFINITIONS HERE...
+  // Enter username
+  if (userType.toLowerCase() === "admin") {
+    await PageManager.loginPage.enterUsername(PageManager.loginPage.adminUsername);
+  } else {
+    await PageManager.loginPage.enterUsername(PageManager.loginPage.studentUsername); 
+  }
+
+  // Enter password
+  if (userType.toLowerCase() === "admin") {
+    await PageManager.loginPage.enterPassword(PageManager.loginPage.adminPassword);
+  } else {
+    await PageManager.loginPage.enterPassword(PageManager.loginPage.studentPassword); 
+  }
+
+  // Click login button
+  await PageManager.loginPage.clickLoginButton();
+});
+
+When('user clicks the {string} button under the account name', async function (buttonName) {
+//   await PageManager.homePage.clickLogoutButton();
+});
+
+Then('user is redirected to the login page with the title {string}', async function (expectedTitle) {
+//   const actualTitle = await PageManager.page.title();
+//   expect(actualTitle).toBe(expectedTitle);
+});
